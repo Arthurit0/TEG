eb03c7950aa7b75c7e83c187aa815a6ee43dfc4f
Arthur José <57883830+Arthurit0@users.noreply.github.com>
2022-06-09 07:44:01 -0300

    Create euler_path.c

---
+++ Exercício - Caminho Euleriano/euler_path.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+    int total = 0, grau, i, j, M[5][5] = {{0, 2, 1, 0, 0}, {2, 0, 1, 0, 0}, {1, 1, 0, 1, 1}, {0, 0, 1, 0, 2}, {0, 0, 1, 2, 0}};
+
+    for (int i = 0; i < 5; i++) {
+        /* code */
+    }
+
+    return 0;
+}
